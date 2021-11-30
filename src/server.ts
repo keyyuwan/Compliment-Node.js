af3601304f1859658c22cff2daf7f8460e376bf9
Key Yu Wan <keyflcbyuwan@gmail.com>
2021-11-30 00:17:39 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/message', (req, res) => {
+  return res.send('Hello World')
+})
+
+app.post('/compliment', (req, res) => {
+  return res.send('Obrigado')
+})
+
+app.listen(3000, () => console.log('Server running on port 3000'))
