f2a1c552c08d02d7d3cb6536a8aae9ed0b3475f7
Key Yu Wan <keyflcbyuwan@gmail.com>
2021-12-22 23:58:35 -0300

    list user compliments sent and received

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,8 @@ import { AuthenticateUserController } from './controllers/AuthenticateUserContro
 import { CreateComplimentController } from './controllers/CreateComplimentController'
 import { CreateTagController } from './controllers/CreateTagController'
 import { CreateUserController } from './controllers/CreateUserController'
+import { ListUserReceivedComplimentsController } from './controllers/ListUserReceivedComplimentsController'
+import { ListUserSentComplimentsController } from './controllers/ListUserSentComplimentsController'
 import { ensureAdmin } from './middlewares/ensureAdmin'
 import { ensureAuthenticated } from './middlewares/ensureAuthenticated'
 
@@ -12,6 +14,10 @@ const createUserController = new CreateUserController()
 const createTagController = new CreateTagController()
 const authenticateUserController = new AuthenticateUserController()
 const complimentController = new CreateComplimentController()
+const listUserSentComplimentsController =
+  new ListUserSentComplimentsController()
+const listUserReceivedComplimentsController =
+  new ListUserReceivedComplimentsController()
 
 router.post('/users', createUserController.handle)
 router.post(
@@ -22,5 +28,16 @@ router.post(
 )
 router.post('/auth', authenticateUserController.handle)
 router.post('/compliments', ensureAuthenticated, complimentController.handle)
+
+router.get(
+  '/users/compliments/sent',
+  ensureAuthenticated,
+  listUserSentComplimentsController.handle
+)
+router.get(
+  '/users/compliments/received',
+  ensureAuthenticated,
+  listUserReceivedComplimentsController.handle
+)
 
 export { router }
